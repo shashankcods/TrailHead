d0a2a8a3579454a48ef5f9c490511c0cb1214537
kyrolxg <71367144+kyrolxg@users.noreply.github.com>
2025-10-05 15:05:59 +0530

    Updated the backend structure

--- trailhead-backend/src/routes/testRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/", (req, res) => {
-    res.json({ message: "Test route is working" });
-}); 
-
-export default router;
