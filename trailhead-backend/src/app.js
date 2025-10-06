38571368f1fdfc9a8e3ee29e9e6f200f1a14f0b0
kyrolxg <71367144+kyrolxg@users.noreply.github.com>
2025-10-06 14:18:28 +0530

    Cleaned up app.js after the merge conflict

--- trailhead-backend/src/app.js
+++ trailhead-backend/src/app.js
@@ -1,12 +1,9 @@
 import express from "express";
 // import cors from "cors";
 import mapsRoute from "./agents/maps/maps.route.js";
-<<<<<<< HEAD
 import orchestratorRoute from "./orchestrator/orchestrator.route.js";
-=======
 import weatherRoute from "./agents/weather/weather.route.js";
 
->>>>>>> 812bee36bc487f7b981aa2c5c67089ee78cc2b0d
 const app = express();
 
 // app.use(cors());
@@ -17,10 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/maps", mapsRoute);
-<<<<<<< HEAD
 app.use("/api/orchestrator", orchestratorRoute);
-=======
 app.use("/api/weather", weatherRoute);
->>>>>>> 812bee36bc487f7b981aa2c5c67089ee78cc2b0d
 
 export default app;
