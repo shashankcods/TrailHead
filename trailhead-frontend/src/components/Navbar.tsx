777910fa4e8d33b30759568d9c10ccd7ec3ad86d
shashank <hankerl256@gmail.com>
2025-10-06 10:44:57 +0530

    UI update (again) and suggestions under Source/Destination

--- trailhead-frontend/src/components/Navbar.tsx
+++ trailhead-frontend/src/components/Navbar.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
     <nav className="flex items-center justify-between px-6 py-4 bg-white/5">
-      <h1 className="text-white text-4xl font-bold font-family-ubuntu">TrailHead</h1>
+      <Link to="/">
+        <h1 className="text-white text-4xl font-bold font-family-ubuntu cursor-pointer">
+          TrailHead
+        </h1>
+      </Link>
     </nav>
   );
 };
