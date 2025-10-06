4a5edd7819e80269462fb2a3ffc47ea16f609399
kyrolxg <71367144+kyrolxg@users.noreply.github.com>
2025-10-06 17:11:38 +0530

    Integrated the weather agent into the orchestrator

--- trailhead-backend/src/orchestrator/orchestrator.service.js
+++ trailhead-backend/src/orchestrator/orchestrator.service.js
@@ -1,19 +1,28 @@
 import { getRouteFromORS } from "../agents/maps/maps.service.js";
+import { getWeatherFromOpenMeteo } from "../agents/weather/weather.service.js";
 
 export const orchestrateTripService = async (tripDetails) => {
   const { source, destination, startDate, endDate, budget } = tripDetails;
 
-  const mapsData = await getRouteFromORS(source, destination);
+  try{
+    const mapsData = await getRouteFromORS(source, destination);
+    const weatherData = await getWeatherFromOpenMeteo(destination, startDate, endDate);
 
-  return {
-    summary: "Trip plan generated successfully!",
-    maps: mapsData,
-    meta: {
-      source,
-      destination,
-      startDate,
-      endDate,
-      budget
-    }
-  };
+    return {
+      summary: "Trip plan generated successfully!",
+      maps: mapsData,
+      weather: weatherData,
+      meta: {
+        source,
+        destination,
+        startDate,
+        endDate,
+        budget
+      },
+    };
+  } 
+  catch(error){
+    console.error("Error in orchestrateTripService: ", error.message);
+    throw error;
+  }  
 };
