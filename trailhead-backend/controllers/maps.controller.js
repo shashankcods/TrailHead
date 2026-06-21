a5c5ae14052df78fa5ce14a9979f2cc58fcd34e0
kyrolxg <71367144+kyrolxg@users.noreply.github.com>
2026-06-21 21:03:48 +0300

    Revert "Merge pull request #88 from shashankcods/feature/71-city-autocomplete"
    This reverts commit 99707b3b648abaca7b72f61dd6f44fa7e95094af, reversing
    changes made to 97d0de66ecb60fef3949258d4b08e926a95d31be.

--- trailhead-backend/controllers/maps.controller.js
+++ trailhead-backend/controllers/maps.controller.js
@@ -1,4 +1,4 @@
-import { getRouteFromORS, getCityAutocomplete } from "../services/maps.service.js";  // Import func from the service
+import { getRouteFromORS } from "../services/maps.service.js";  // Import func from the service
 import APIResponse from "../utils/APIResponse.js"
 import asyncHandler from "../utils/asyncHandler.js"
 
@@ -15,14 +15,4 @@ export const getRoute = asyncHandler(async (req, res) => {
   const routeData = await getRouteFromORS(from, to);
   res.status(200).json(new APIResponse(200, routeData, "Route data fetched successfully"));
 
-});
-
-export const autocompleteCity = asyncHandler(async (req, res) => {
-  const { query } = req.query;
-  if (!query) {
-    return res.status(400).json({ error: "Missing 'query' parameter" });
-  }
-
-  const suggestions = await getCityAutocomplete(query);
-  res.status(200).json(new APIResponse(200, suggestions, "Autocomplete suggestions fetched successfully"));
 });
